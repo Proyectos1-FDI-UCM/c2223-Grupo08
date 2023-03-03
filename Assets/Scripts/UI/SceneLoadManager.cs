f37db9a6ebea15e7a0da4ee2467351869b9e4373
Usuario Local <damore11@ucm.es>
2023-03-03 13:15:38 +0100

    transicion

---
+++ Assets/Scripts/UI/SceneLoadManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    private Animator transitionAnimator;
+    void Start()
+    {
+        transitionAnimator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (true)
+        {
+            LoadNextScene();
+        }
+    }
+    void LoadNextScene()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        StartCoroutine(SceneLoad(nextSceneIndex));
+    }
+
+    public IEnumerator SceneLoad(int sceneIndex)
+    {
+        transitionAnimator.SetTrigger("StartTransition");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
