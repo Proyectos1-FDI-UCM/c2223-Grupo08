3093bc6721ccf53c76b9af283bbc13b62205adc6
miguelgonzgrande <125408087+miguelgonzgrande@users.noreply.github.com>
2023-03-07 22:34:32 +0100

    Animacion boton

--- Assets/Scripts/Items/BotonComponent.cs
+++ Assets/Scripts/Items/BotonComponent.cs
@@ -9,15 +9,27 @@ public class BotonComponent : MonoBehaviour
 
     private bool _activated = false;
 
+    private Animator _animator;
+    private void Start()
+    {
+        _animator= GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         targetGameObject.SendMessage("ActivateGameObject"); //Llama a la funcion del gameobject, tiene que tener una funcion con ese nombre para ser activado
         _activated = true;
+
+        _animator.SetBool("IsPressed", true);
     }
 
     public void ResetBoton()
     {
         targetGameObject.SendMessage("DeactivateGameObject"); //Llama a la funcion del gameobject, tiene que tener una funcion con ese nombre para ser activado
         _activated = false;
+
+        _animator.SetBool("IsPressed", false);
     }
+
+
 }
