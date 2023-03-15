53186145ff2fccb055abbd74459c5baa0f3a0a39
Diego <dialon04@ucm.es>
2023-03-15 19:44:13 +0100

    Animación Roca 1.0

--- Assets/Scripts/Items/RockComponent.cs
+++ Assets/Scripts/Items/RockComponent.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class RockComponent : MonoBehaviour
 {
+
+    private Animator _anim;
+
+    private void Awake()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _anim.SetTrigger("Destruido");
+        Invoke("Destruir", 1);
+    }
+
+    private void Destruir()
     {
         Destroy(gameObject);
     }
