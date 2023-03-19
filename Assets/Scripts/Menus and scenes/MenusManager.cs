d0eff13220331fcde1ce6719bdf671ab0d96cf2f
nico-best1 <nicobest@ucm.es>
2023-03-19 10:59:56 +0100

    Pausa con un pequeño error

--- Assets/Scripts/Menus and scenes/MenusManager.cs
+++ Assets/Scripts/Menus and scenes/MenusManager.cs
@@ -19,7 +19,8 @@ public class MenusManager : MonoBehaviour
     }
     public void ChangeToGameScene() //Carga la escena de juego desde el menu una vez pulsado el boton
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("GameScene 1");
+        Time.timeScale = 1.0f;
     }
 
     public void QuitGame() // Sale del juego una vez al pulsar boton de salir
@@ -31,8 +32,14 @@ public class MenusManager : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
-    public void PauseGame()
+    public void PauseGame() // Pausa el juego y abre el menu de pausa
     {
-        SceneManager.LoadSceneAsync("Pausa");
+        SceneManager.LoadSceneAsync("Pausa", LoadSceneMode.Additive);
+        Time.timeScale= 0f;
+    }
+   public void UnpauseGame() // Reanuda el juego cerrando el menu de pausa
+    {
+        SceneManager.UnloadSceneAsync("Pausa");
+        Time.timeScale = 1.0f;
     }
 }
