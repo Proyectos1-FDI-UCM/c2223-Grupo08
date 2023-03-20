a62c54cdbe7827611327c5363dc0c7b075990561
PedroSanchezVela06 <pedros06@ucm.es>
2023-03-20 21:33:27 +0100

    arreglando lo que otros rompen

--- Assets/Scripts/Items/EatingBallComponent.cs
+++ Assets/Scripts/Items/EatingBallComponent.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class EatingBallComponent : MonoBehaviour
 {
+    private int _count = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (PlayerManager.Instance.getSize() < 5)   //Impide que obtenga mas de 5 bolas
+        if (PlayerManager.Instance.getSize() < 5 && _count == 0)   //Impide que obtenga mas de 5 bolas
         {
             Destroy(gameObject);
             PlayerManager.Instance.incrementSize();
+            _count++;
         }
     }
 }
