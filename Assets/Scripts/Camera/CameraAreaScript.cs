c5e5c607b92cada623fec49e26038ba3dfcf45d7
PedroSanchezVela06 <pedros06@ucm.es>
2023-04-11 02:37:17 +0200

    Comentado CameraAreaScript

--- Assets/Scripts/Camera/CameraAreaScript.cs
+++ Assets/Scripts/Camera/CameraAreaScript.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class CameraAreaScript : MonoBehaviour
 {
+    #region properties
     [TextArea]
     [Tooltip("No hace nada, solo es un comentario.")]
     public string Notes = "Poner a 7 tiles de distancia de las paredes verticales para no ver a traves de ellas.";
 
+    /// <summary>
+    /// Collider de este gameobject
+    /// </summary>
     private Collider2D m_Collider;
+
+    /// <summary>
+    /// Referencia a la camera movement de la main camera
+    /// </summary>
     private CameraMovement _cameraMovement;
+
+    /// <summary>
+    /// El transform del player
+    /// </summary>
     private Transform _playerTransform;
+    #endregion
+
+    #region methods
+    /// <summary>
+    /// Encuentra el punto mas cercano al area de uno dado
+    /// </summary>
+    /// <param name="point">El punto ha encontrar</param>
+    /// <returns>El punto mas cercano a este area</returns>
+    public Vector2 ClosestPoint(Vector2 point)
+    {
+        return m_Collider.bounds.ClosestPoint(point);
+    }
+    #endregion
 
     private void Awake()
     {
@@ -34,10 +59,5 @@ public class CameraAreaScript : MonoBehaviour
         {
             _cameraMovement.UpdateTargetPositionY();
         }
-    }
-
-    public Vector2 ClosestPoint(Vector2 point)
-    {
-        return m_Collider.bounds.ClosestPoint(point);
     }
 }
