c287082ff4a946c3a74040c5007e8081f8b1a926
PedroSanchezVela06 <pedros06@ucm.es>
2023-04-11 23:27:07 +0200

    Comentado SceneLoader

--- Assets/Scripts/UI/SceneLoadManager.cs
+++ Assets/Scripts/UI/SceneLoadManager.cs
@@ -5,23 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoadManager : MonoBehaviour
 {
+    #region references
+    /// <summary>
+    /// Si es true la siguiente escena en cargar es el main menu
+    /// </summary>
     [Tooltip("True para que la siguiente escena sea el main menu")]
     public bool NextSceneMenu = false;
 
+    /// <summary>
+    /// PlayerAnimator del player
+    /// </summary>
     [SerializeField]
     private PlayerAnimator _playerAnimator;
-    private Animator transitionAnimator;
-    void Start()
-    {
-        transitionAnimator = GetComponentInChildren<Animator>();
+    #endregion
 
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        PlayerManager.Instance.EnableInputs(false);
-        _playerAnimator.LoadScene();
-        LoadNextScene();
-    }
+    #region properties
+    /// <summary>
+    /// Referencia del animador
+    /// </summary>
+    private Animator transitionAnimator;
+    #endregion
+
+    #region methods
+    /// <summary>
+    /// Carga la siguiente escena
+    /// </summary>
     void LoadNextScene()
     {
         int nextSceneIndex;
@@ -36,10 +44,30 @@ public class SceneLoadManager : MonoBehaviour
         StartCoroutine(SceneLoad(nextSceneIndex));
     }
 
+    /// <summary>
+    /// Carga la escena
+    /// </summary>
+    /// <param name="sceneIndex">La escena a cargar</param>
+    /// <returns></returns>
+    //Ver si se puede quitar la corutina
     public IEnumerator SceneLoad(int sceneIndex)
     {
         transitionAnimator.SetTrigger("StartTransition");
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(sceneIndex);
     }
+    #endregion
+
+    void Start()
+    {
+        transitionAnimator = GetComponentInChildren<Animator>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerManager.Instance.EnableInputs(false);
+        _playerAnimator.LoadScene();
+        LoadNextScene();
+    }
+    
 }
