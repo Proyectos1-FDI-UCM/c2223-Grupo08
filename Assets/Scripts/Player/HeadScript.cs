be65fbe75e98bfb0a03a1690242a2059547a772e
PedroSanchezVela06 <pedros06@ucm.es>
2023-04-11 22:51:51 +0200

    Comentado HeadScript

--- Assets/Scripts/Player/HeadScript.cs
+++ Assets/Scripts/Player/HeadScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class HeadScript : MonoBehaviour
 {
+    #region references
+    /// <summary>
+    /// Referencia al InputController del jugador
+    /// </summary>
     [SerializeField]
     private InputController _inputs;
+    #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
