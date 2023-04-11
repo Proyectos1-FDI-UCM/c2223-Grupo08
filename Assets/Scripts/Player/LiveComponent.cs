157b492bf83a0a4b58ec3adc21a09479d74f51f8
PedroSanchezVela06 <pedros06@ucm.es>
2023-04-11 22:55:31 +0200

    Comentado LiveComponent

--- Assets/Scripts/Player/LiveComponent.cs
+++ Assets/Scripts/Player/LiveComponent.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class LiveComponent : MonoBehaviour
 {
+    #region properties
+    /// <summary>
+    /// Referencia al animador
+    /// </summary>
     private Animator _animator;
+    #endregion
 
     private void Start()
     {
         _animator= GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Mata al jugador
+    /// </summary>
+    // Mirar si se puede quitar el SendMessage
     public void Death ()
     {
         PlayerManager.Instance.SetAlive(false);
@@ -19,6 +28,10 @@ public class LiveComponent : MonoBehaviour
         Invoke("PlayDeath", 1); //Llama a la funcion del gameobject, tiene que tener una funcion con ese nombre para ser activado
     }
 
+    /// <summary>
+    /// Reinicia la sala
+    /// </summary>
+    // Mirar si se puede quitar la corutina
     private void PlayDeath()
     {
         StartCoroutine(GameManager.Instance.ResetRoom());
