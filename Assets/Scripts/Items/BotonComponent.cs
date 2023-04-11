25d4bec034119d07cc38e166b52a6722929ed4a9
PedroSanchezVela06 <pedros06@ucm.es>
2023-04-11 03:04:47 +0200

    Comentado BotonComponent

--- Assets/Scripts/Items/BotonComponent.cs
+++ Assets/Scripts/Items/BotonComponent.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class BotonComponent : MonoBehaviour
 {
+    #region references
+    /// <summary>
+    /// El objeto que va a activar
+    /// </summary>
     [SerializeField]
-    private GameObject targetGameObject;    //El objeto que va a activar
+    private GameObject targetGameObject;
+    #endregion
 
+    #region properties
+    /// <summary>
+    /// Indica si esta activado
+    /// </summary>
     private bool _activated = false;
 
+    /// <summary>
+    /// Referencia al animador
+    /// </summary>
     private Animator _animator;
+    #endregion
+
+    #region methods
+    /// <summary>
+    /// Reinicia el boton y pasa a estar desactivado
+    /// </summary>
+    public void ResetBoton()
+    {
+        targetGameObject.SendMessage("DeactivateGameObject"); //Llama a la funcion del gameobject, tiene que tener una funcion con ese nombre para ser activado
+        _activated = false;
+
+        _animator.SetBool("IsPressed", false);
+    }
+    #endregion
+
     private void Awake()
     {
         _animator= GetComponent<Animator>();
@@ -21,13 +48,5 @@ public class BotonComponent : MonoBehaviour
         _activated = true;
 
         _animator.SetBool("IsPressed", true);
-    }
-
-    public void ResetBoton()
-    {
-        targetGameObject.SendMessage("DeactivateGameObject"); //Llama a la funcion del gameobject, tiene que tener una funcion con ese nombre para ser activado
-        _activated = false; 
-
-        _animator.SetBool("IsPressed", false);
     }
 }
