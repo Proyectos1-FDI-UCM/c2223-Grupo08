8946da51ee9eda127950ffdf8ffa195b0505f160
PedroSanchezVela06 <pedros06@ucm.es>
2023-04-11 02:28:13 +0200

    Comentando GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,46 +7,83 @@ using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
+    # region references
+    /// <summary>
+    /// UI manager del juego
+    /// </summary>
     [SerializeField]
     private UIManager _uiManager;
+
+    /// <summary>
+    /// Los frames a los que corre el juego
+    /// </summary>
     [SerializeField]
     private int targetFrameRate = 120;
+
+    /// <summary>
+    /// Gameobject con los gameobjects de las cajas
+    /// </summary>
     [SerializeField]
     private GameObject _boxes;
+
+    /// <summary>
+    /// Gameobject con los gameobjects de las bolas
+    /// </summary>
     [SerializeField]
     private GameObject _balls;
+
+    /// <summary>
+    /// Gameobject con los gameobjects de los botones
+    /// </summary>
     [SerializeField]
     private GameObject _buttons;
+
+    /// <summary>
+    /// Gameobject con los gameobjects de las areas de las camaras
+    /// </summary>
     [SerializeField]
     private GameObject _cameraAreas;
+
+    /// <summary>
+    /// Gameobject con los gameobjects de las puertas
+    /// </summary>
     [SerializeField]
     private GameObject _doors;
+    #endregion
 
+    #region properties
+    /// <summary>
+    /// La sala en la que se encuentra el jugador actualmente
+    /// </summary>
     private int _currentRoom = 0;
+    /// <summary>
+    /// Referencia al player manager
+    /// </summary>
     private PlayerManager _playerManager;
 
+    /// <summary>
+    /// La instancia del game manager
+    /// </summary>
     static private GameManager _instance;
-    static public GameManager Instance { get { return _instance; } }
 
+    /// <summary>
+    /// Indica si esta pausado el juego
+    /// </summary>
     public bool isPaused = false;
 
+    /// <summary>
+    /// Indica si esta enseñando la puerta abriendose
+    /// </summary>
     public bool IsShowingOpenDoor = false;
 
-    private void Awake()
-    {
-        _instance = this;
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = targetFrameRate;
-    }
+    static public GameManager Instance { get { return _instance; } }
 
-    private void Start()
-    {
-        _playerManager = PlayerManager.Instance;
-        _currentRoom = SaveScript.room;
-        Init();
-    }
+    #endregion
 
-    //Inicializa todo lo necesario antes de jugar
+    #region methods
+    ///<summary>
+    ///Inicializa todo lo necesario antes de jugar
+    ///</summary>
     private void Init()
     {
         _playerManager.EnableInputs(false);
@@ -69,7 +106,10 @@ public class GameManager : MonoBehaviour
         _playerManager.EnableInputs(true);
     }
 
-    //Comprueba en cada caja si debe liberarse o no
+    ///<summary>
+    ///Comprueba en cada caja si debe liberarse o no
+    ///</summary>
+    ///<param name="size">El tamaño del jugador</param>
     public void CheckBoxes(int size) {
         BoxManager[] boxManagers = _boxes.GetComponentsInChildren<BoxManager>();
         if (boxManagers.Length != 0)
@@ -81,7 +121,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //Reinicia las cajas a su posición inicial
+    ///<summary>
+    ///Reinicia las cajas a su posición inicial
+    ///</summary>
     public void ResetBoxes()
     {
         SpawnItem[] spawnManagers = _boxes.GetComponentsInChildren<SpawnItem>();
@@ -94,7 +136,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //Reinicia las bolas a su posición inicial
+    ///<summary>
+    ///Reinicia las bolas a su posición inicial
+    ///<summary>
     public void ResetBalls()
     {
         SpawnItem[] spawnManagers = _balls.GetComponentsInChildren<SpawnItem>();
@@ -107,7 +151,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //Reinicia las botones a su posición inicial
+    ///<summary>
+    ///Reinicia las botones a su posición inicial
+    ///<summary>
     public void ResetButtons()
     {
         BotonComponent[] spawnManagers = _buttons.GetComponentsInChildren<BotonComponent>();
@@ -119,8 +165,10 @@ public class GameManager : MonoBehaviour
             }
         }
     }
-    
-    //Reinicia las puertas a como si fuese la primera vez que se abren
+
+    ///<summary>
+    ///Reinicia las puertas a como si fuese la primera vez que se abren
+    ///<summary>
     public void ResetDoors()
     {
         DoorComponent[] components = _doors.GetComponentsInChildren<DoorComponent>();
@@ -133,7 +181,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //Reinicia las botones a su posición inicial
+    ///<summary>
+    ///Reinicia las botones a su posición inicial
+    ///<summary>
     public void ResetSaws()
     {
         SawComponent[] spawnManagers = _buttons.GetComponentsInChildren<SawComponent>();
@@ -146,7 +196,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //Reinicia los objetos y el personaje de la sala con una pequeña animación
+    ///<summary>
+    ///Reinicia los objetos y el personaje de la sala con una pequeña animación
+    ///<summary>
+
+    //Necesario revision
     public IEnumerator ResetRoom()
     {
         PlayerManager.Instance.EnableInputs(false);
@@ -174,6 +228,12 @@ public class GameManager : MonoBehaviour
         _playerManager.SetAlive(true);
     }
 
+    ///<summary>
+    ///Mueve la camara a la siguiente sala
+    ///</summary>
+    ///<param name="door">La puerta entre la sala actual y la siguiente</param>
+
+    //Cambiar el nombre a uno mejor
     public void nextRoom(DoorComponent door)
     {
         _currentRoom++;
@@ -182,13 +242,35 @@ public class GameManager : MonoBehaviour
         _playerManager.moveToNextRoom(_currentRoom, _cameraAreas.GetComponentsInChildren<CameraAreaScript>()[_currentRoom].ClosestPoint(_playerManager.getSpawnPoint(_currentRoom)), door);
     }
 
+    ///<summary>
+    ///Guarda la partida
+    ///</summary>
     public void saveGame()
     {
         SaveScript.SaveFile(_currentRoom, SceneManager.GetActiveScene().name);
     }
 
+
+    ///<summary>
+    ///Ajusta las bolas de la UI al indicado
+    ///</summary>
     public void ResizeBallsBar(int size)
     {
         _uiManager.ResizeBallsBar(size);
+    }
+    #endregion
+
+    private void Awake()
+    {
+        _instance = this;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+
+    private void Start()
+    {
+        _playerManager = PlayerManager.Instance;
+        _currentRoom = SaveScript.room;
+        Init();
     }
 }
