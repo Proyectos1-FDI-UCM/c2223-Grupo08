8575b0ff4c2e47e38d736be596b5e3ec3cbce1b2
PedroSanchezVela06 <pedros06@ucm.es>
2023-04-11 19:51:25 +0200

    Comentado DoorComponent

--- Assets/Scripts/Items/DoorComponent.cs
+++ Assets/Scripts/Items/DoorComponent.cs
@@ -4,18 +4,125 @@ using UnityEngine;
 
 public class DoorComponent : MonoBehaviour
 {
-    private bool _isFirstTime = false;
-    private CameraMovement CameraMovementComponent;
+    #region references
+    /// <summary>
+    /// Referencia al siguiente area de la camara
+    /// </summary>
     [SerializeField]
     private CameraAreaScript _CameraArea;
 
-    private Animator _animator;
-    private BoxCollider2D _collider;
-
-    private Vector2 DoorPosition;
-
+    /// <summary>
+    /// Indica si debe estar abierta desde el principio
+    /// </summary>
     [SerializeField]
     private bool _isOpenFromBeginning = false;
+    #endregion
+
+    #region properties
+    /// <summary>
+    /// Indica si es la primera vez que se abre
+    /// </summary>
+    private bool _isFirstTime = false;
+
+    /// <summary>
+    /// Referencia al CameraMovement de la camara principal
+    /// </summary>
+    private CameraMovement CameraMovementComponent;
+
+    /// <summary>
+    /// Referencia al animador
+    /// </summary>
+    private Animator _animator;
+
+    /// <summary>
+    /// Referencia al collider2D de la puerta
+    /// </summary>
+    private BoxCollider2D _collider;
+
+    /// <summary>
+    /// La posicion de la puerta
+    /// </summary>
+    private Vector2 DoorPosition;
+    #endregion
+
+    #region methods
+
+    /// <summary>
+    /// Abre la puerta
+    /// </summary>
+    private void ActivateGameObject()
+    {
+        OpenDoor();
+    }
+
+    /// <summary>
+    /// Cierra la puerta
+    /// </summary>
+    private void DeactivateGameObject()
+    {
+        CloseDoor();
+    }
+
+    /// <summary>
+    /// Vuelve a dejar la puerta como si fuera la primera vez que se abre
+    /// </summary>
+    public void resetFirstTime()
+    {
+        _isFirstTime = false;
+    }
+
+    /// <summary>
+    /// Abre la puerta y si es la primera vez que se abre, hace una pequeña animacion
+    /// </summary>
+    public void OpenDoor()
+    {
+        if (_isFirstTime == false)
+        {
+            CameraAnimToDoor();
+        }
+        _collider.enabled = false;
+        _animator.SetBool("isOpen", true);
+        _isFirstTime = true;
+    }
+
+    /// <summary>
+    /// Cierra la puerta
+    /// </summary>
+    public void CloseDoor()
+    {
+        _collider.enabled = true;
+        _animator.SetBool("isOpen", false);
+    }
+
+    /// <summary>
+    /// Redirige la camara a la puerta
+    /// </summary>
+    public void CameraToDoor()
+    {
+        CameraMovementComponent.MoveCamera(DoorPosition);
+        GameManager.Instance.IsShowingOpenDoor = true;
+        PlayerManager.Instance.EnableInputs(false);
+    }
+
+    /// <summary>
+    /// Devuelve la camara al jugador
+    /// </summary>
+    public void CameraReturnToPlayer()
+    {
+        CameraMovementComponent.MoveCamera(_CameraArea.ClosestPoint(PlayerManager.Instance.transform.position));
+        GameManager.Instance.IsShowingOpenDoor = false;
+        PlayerManager.Instance.EnableInputs(true);
+    }
+
+    /// <summary>
+    /// Manda la camara a la puerta y ,despues de un tiempo, la devuelve al jugador
+    /// </summary>
+    private void CameraAnimToDoor()
+    {
+        CameraToDoor();
+        Invoke("CameraReturnToPlayer", 2f);
+    }
+    #endregion
 
     private void Awake()
     {
@@ -30,54 +137,4 @@ public class DoorComponent : MonoBehaviour
         }
     }
 
-    private void ActivateGameObject()
-    {
-        OpenDoor();
-    }
-    private void DeactivateGameObject()
-    {
-        CloseDoor();
-    }
-
-    public void resetFirstTime()
-    {
-        _isFirstTime = false;
-    }
-
-    public void OpenDoor()
-    {
-        if (_isFirstTime == false)
-        {
-            CameraAnimToDoor();
-        }
-        _collider.enabled = false;
-        _animator.SetBool("isOpen", true);
-        _isFirstTime = true;
-    }
-
-    public void CloseDoor()
-    {
-        _collider.enabled = true;
-        _animator.SetBool("isOpen", false);
-    }
-
-    public void CameraToDoor()
-    {
-        CameraMovementComponent.MoveCamera(DoorPosition);
-        GameManager.Instance.IsShowingOpenDoor= true;
-        PlayerManager.Instance.EnableInputs(false);
-    }
-
-    public void CameraReturnToPlayer()
-    {
-        CameraMovementComponent.MoveCamera(_CameraArea.ClosestPoint(PlayerManager.Instance.transform.position));
-        GameManager.Instance.IsShowingOpenDoor = false;
-        PlayerManager.Instance.EnableInputs(true);
-    }
-
-    private void CameraAnimToDoor()
-    {
-        CameraToDoor();
-        Invoke("CameraReturnToPlayer", 2f);
-    }
 }
